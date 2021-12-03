0563db2af6b8f60e7a38d6fcae19dd03eb713a66
buebue <66698690+buebuelol@users.noreply.github.com>
2021-12-03 20:31:57 +0100

    Add files via upload

---
+++ functions.js
@@ -0,0 +1,109 @@
+const Discord = require('discord.js')
+const client = new Discord.Client()
+const config = require("./config.json")
+const json = JSON.parse(LoadResourceFile('js', 'requests.json'))
+
+client.on("ready", () => {
+  console.log('bot on')
+  client.setInterval(checkTimestamps, 30000)
+})
+
+function checkRole(gid, uid, rid) { // funkcja odpowiada za sprawdzanie czy uzytkownik ma podana role
+  var gowno = false
+  if(!gid || !uid || !rid) return console.log("missing arguments!")
+  if(client.guilds.get(gid).members.get(uid).roles.get(rid)) gowno = true 
+  return gowno
+}
+
+function checkTimestamps() { // funkcja odpowiada za usuwanie przedawnionych kodow
+  for(let i in json) {
+    let timestamp = json[i].timestamp
+    if(Date.now() / 1000 > timestamp) {
+      delete json[i]
+    }
+  }
+  SaveResourceFile('buebue_verify', 'requests.json', JSON.stringify(json, null, 4), -1)
+}
+
+function genCode() { return Math.floor(Math.random() * (999999 - 100000 + 1)) + 100000 } // funkcja odpowiadajaca za wygenerowanie kodu
+
+function getUser(gid, uid) { // funkcja odpowiada za zdobycie informacji o uzytkowniku
+  if (!gid || !uid) return console.log("missing arguments!")
+  let guild = client.guilds.get(gid)
+  if(!guild) return console.log('# podany guild nie został znaleziony')
+  let member = guild.members.get(uid)
+  if(!member) return false; else return true
+}
+
+function createJSON(userid, code) { // funkcja odpowiadajaca za stworzenie objectu w pliku json
+  if(json[userid] && json[userid].remember == false) {
+    delete json[userid]
+    SaveResourceFile('js', 'requests.json', JSON.stringify(json, null, 4), -1)
+  } else {
+      json[userid] = {
+        code: code,
+        remember: false,
+        timestamp: Math.floor(Date.now() + 21600000) / 1000
+      }
+      SaveResourceFile('js', 'requests.json', JSON.stringify(json, null, 4), -1)
+  }
+}
+
+function checkRM(userid) { // funkcja odpowiadajaca za sprawdzanie czy uzytkownik zapamietal juz wczesniej swoj kod
+  if(!userid) return console.log("missing arguments!")
+  if(!json[userid]) return 
+  return json[userid].remember
+}
+
+function updateJSON(userid, code) { // funkcja odpowiadajaca za nadpisywanie objectu w pliku json
+  if(json[userid]) return; else {
+    json[userid] = {
+      code: code
+    }
+    SaveResourceFile('js', 'requests.json', JSON.stringify(json), -1)
+  }
+}
+
+function sendLog(channel, embed, message) { // funckcja odpowiadajaca za wysyłanie wiadomosic gdy ktos polaczy sie z serwerem
+  if(!channel || !message) return console.log("missing arguments!")
+  let channell = client.channels.get(channel)
+  if(!channell) return console.log('[SENDLOG]: kanał o id ' + channel + ' nie został znaleziony')
+  if(embed == true) {
+    const embed = new Discord.RichEmbed().setAuthor('Kolejka - Logi').setTitle('Uzytkownik łączy sie z serwerem').setColor('#00ff48').setDescription(message)
+    return channell.send(embed).catch((err) => {
+      throw err;
+    })
+  } else {
+    channell.send(message).catch((err) => {
+      throw err;
+    })
+  }
+}
+
+function deleteJSON(userid) { // funkcja odpowiadajaca za usuwanie objectu gdy uzytkownik juz sie polaczy i nie zapamietal kodu
+  if(!userid) return console.log("missing arguments!")
+  if(!json[userid]) return
+  delete json[userid]
+  SaveResourceFile('js', 'requests.json', JSON.stringify(json), -1)
+}
+
+function checkCode(userid, code) { // funkcja sprawdzajaca czy podany kod jest prawidłowy
+  var valid = true
+  if(!userid || !code) return console.log("missing arguments!")
+  if(json[userid].code != code) valid = false
+  return valid
+}
+
+function sendVerifyCode(gid, uid, code)  { // funkcja odpowiadajaca za wyslanie wiadomosci z kodem do uzytkownika
+  if(!gid || !uid || !code) return console.log("missing arguments!")
+  let member = client.guilds.get(gid).members.get(uid)
+  if(member) console.log('znaleziono uzytkownika'); else {
+    return console.log('nie znaleziono uzytkownika')
+  }
+  member.send('Twój kod weryfikacyjny: ``' + code + '``').catch(() => {})
+}
+
+module.exports = { checkRole, getUser, genCode, checkRM, createJSON, updateJSON, sendLog, deleteJSON, checkCode, sendVerifyCode }
+
+if(!config.token) return console.log('config: nie wprowadzono tokenu');
+client.login(config.token).catch(() => { console.log('bot: podany token jest nieprawidłowy')})
---
+++ queue.js
@@ -0,0 +1,75 @@
+const config = require('./config.json')
+const functions = require('./functions.js')
+
+function sleep(time) {
+    return new Promise((resolve) => setTimeout(resolve, time));
+}
+
+on('playerConnecting', async (name, reason, deferrals) => {
+    let user = GetIdentifiers(source)
+    if(user.discord == 'Brak') return deferrals.done('# nie znaleziono twojego discordid!')
+    deferrals.defer()
+    await sleep(1)
+    deferrals.update('# oczekuj, jestes sprawdzany')
+    await sleep(3000)
+    //console.log(user)
+    if(functions.getUser(config.guildid, user.discord) == false) return deferrals.done('# nie znajdujesz sie na discordzie')
+    deferrals.update('# znajdujesz sie na disordzie')
+    await sleep(500)
+    deferrals.update('# trwa sprawdzanie rang')
+    await sleep(1000)
+    if(functions.checkRole(config.guildid, user.discord, config.roleid) == false) return deferrals.done('# nie posiadasz whitelisty!')
+    deferrals.update('# trwa tworzenie polaczenia')
+    await sleep(500)
+    if(functions.checkRM(user.discord) == true) {
+        deferrals.update('# zweryfikowano cie z wcześniej zapisanego kodu')
+        await sleep(500)
+        deferrals.update('# trwa laczenie z serwerem')
+        deferrals.done()
+        functions.sendLog(config.logchannelid, true, '```STEAM: ' + user.steam + '\nIP: ' + user.ip + '\nDISCORD: ' + user.discord + '\nLICENSE: ' + user.license + '\nXBL: ' + user.xbl + '\nLIVE: ' + user.live + '```')
+        functions.deleteJSON(user.discord)
+        return
+    }
+    let code = functions.genCode()
+    functions.sendVerifyCode(config.guildid, user.discord, code)
+    functions.createJSON(user.discord, code)
+    deferrals.update("# kod został przesłany!")
+    await sleep(500)
+    let card = '{"type":"AdaptiveCard","body":[{"type":"TextBlock","size":"ExtraLarge","weight":"Bolder","text":"Weryfikacja"},{"type":"TextBlock","text":"Podaj kod z Discorda który przesłał ci nasz bot!","wrap":true},{"type":"Input.Text","id":"password","title":"","placeholder":"000000","isRequired":true,"errorMessage":"Kod jest wymagany!"},{"type":"Input.Toggle", "title":"Zapamiętaj kod", "id":"remember"},{"type":"ActionSet","actions":[{"type":"Action.Submit","title":"POTWIERDZ","iconUrl":"https://i.imgur.com/J1c755g.png","style":"positive"}]}],"$schema":"http://adaptivecards.io/schemas/adaptive-card.json","version":"1.2"}'
+    deferrals.presentCard(card, async (data, rawData) => {
+        if(functions.checkCode(user.discord, data.password) == false) {
+            deferrals.done('# podany kod jest nieprawidlowy')
+        } else {
+            if(data.remember == 'true') updateJSON(user.discord, true)
+            deferrals.update('# trwa laczenie z serwerem')
+            await sleep(500)
+            deferrals.done()
+            functions.sendLog(config.logchannelid, true, '```STEAM: ' + user.steam + '\nIP: ' + user.ip + '\nDISCORD: ' + user.discord + '\nLICENSE: ' + user.license + '\nXBL: ' + user.xbl + '\nLIVE: ' + user.live + '```')
+            functions.deleteJSON(user.discord)
+        }
+        //console.log(data, rawData)
+    })
+});
+
+function GetIdentifiers(source) {
+    const identifiers = {
+        steam: "Brak",
+        ip: "Brak",
+        discord: "Brak",
+        license: "Brak",
+        xbl: "Brak",
+        live: "Brak"
+    }
+
+    for (let i = 0; i < GetNumPlayeconfig.roleidentifiers(source); i++) {
+        const identifier = GetPlayeconfig.roleidentifier(source, i);
+        if (identifier.includes('steam:')) identifiers.steam = identifier;
+        if (identifier.includes('ip:')) identifiers.ip = identifier;
+        if (identifier.includes('discord:')) identifiers.discord = identifier.substring(8, identifier.length);
+        if (identifier.includes('license:')) identifiers.license = identifier;
+        if (identifier.includes('xbl:')) identifiers.xbl = identifier;
+        if (identifier.includes('live:')) identifiers.live = identifier;
+    }
+
+    return identifiers;
+}
