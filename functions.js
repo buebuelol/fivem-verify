1ad5b0893dbd9da591db87a7c60303a437c56165
buebue <66698690+buebuelol@users.noreply.github.com>
2022-01-31 23:45:02 +0100

    fix

--- functions.js
+++ functions.js
@@ -1,5 +1,6 @@
 const Discord = require('discord.js')
 const client = new Discord.Client()
+const config = require('./config.json')
 const scriptname = GetCurrentResourceName()
 const json = JSON.parse(LoadResourceFile(scriptname, 'requests.json'))
 
